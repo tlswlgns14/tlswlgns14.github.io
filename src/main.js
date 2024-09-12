f7695e51c31c997b8d55a7618a2930ca4691c73a
tlswlgns14 <85921909+tlswlgns14@users.noreply.github.com>
2024-09-13 01:21:25 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 const header = document.querySelector('.header');
 console.dir(header);
 console.log(header);
-const headerHeight = header.getBoundingClientRect().height;
+// getBoundingClientRect() 소수점까지 나타낼 수 있음
+// const headerHeight = header.getBoundingClientRect().height;
+//offsetHeight 소수점 반올림 한 높이를 나타내줌
+const headerHeight = header.offsetHeight;
 console.log(headerHeight);
 document.addEventListener('scroll', () => {
   console.log(window.scrollY);
@@ -13,4 +16,10 @@ document.addEventListener('scroll', () => {
   }
 })
 
-
+// Home 섹션을 아래로 스크롤 시 투명하게 처리함
+const home = document.querySelector('.home__container')
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  console.log(1 - window.scrollY / homeHeight)
+  home.style.opacity = 1 - window.scrollY / homeHeight
+});
