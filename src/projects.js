b759207027175e430e09bf3a5ee871a376633c79
tlswlgns14 <85921909+tlswlgns14@users.noreply.github.com>
2024-09-15 02:26:24 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,2 +1,37 @@
 'use strict';
 
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if(filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+    // Active  메뉴를 재설정
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    target.classList.add('category--selected');
+};
+
+function filterProjects(filter) {
+    // 프로젝트를 필터링
+    projectsContainer.classList.add('animation-out')
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if(filter === 'all' || filter === project.dataset.type) {
+        project.style.display ='block';
+      } else {
+        project.style.display = 'none';
+      }
+    });
+    setTimeout(() =>{
+      projectsContainer.classList.remove('animation-out')
+    }, 250);
+}
