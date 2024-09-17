300a92f2b25fcfeb9c56d57b26d1d479d713c39f
tlswlgns14 <85921909+tlswlgns14@users.noreply.github.com>
2024-09-17 23:25:34 +0900

    ObserverPart1

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@
 
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
-console.dir(header);
-console.log(header);
+// console.dir(header);
+// console.log(header);
 // getBoundingClientRect() 소수점까지 나타낼 수 있음
 // const headerHeight = header.getBoundingClientRect().height;
 //offsetHeight 소수점 반올림 한 높이를 나타내줌
 const headerHeight = header.offsetHeight;
-console.log(headerHeight);
+// console.log(headerHeight);
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
+  // console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add('header--dark')
   } else {
